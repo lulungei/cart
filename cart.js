9e2e110ee5aabe1d970cfbfa0f728f137da7486c
omingei <ngeilulu@gmail.com>
2018-10-17 16:49:10 +0300

    stripe

--- cart.js
+++ cart.js
@@ -71,42 +71,17 @@ function fixStepIndicator(n) {
   x[n].className += " active";
 }
 
-var card = new Card({
-    // a selector or DOM element for the form where users will
-    // be entering their information
-    form: 'form', // *required*
-    // a selector or DOM element for the container
-    // where you want the card to appear
-    container: '.card-wrapper', // *required*
 
-    formSelectors: {
-        numberInput: 'input#number', // optional — default input[name="number"]
-        expiryInput: 'input#expiry', // optional — default input[name="expiry"]
-        cvcInput: 'input#cvc', // optional — default input[name="cvc"]
-        nameInput: 'input#name' // optional - defaults input[name="name"]
-    },
+var stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
 
-    width: 200, // optional — default 350px
-    formatting: true, // optional - default true
+//token is created using checkout or elements;
+//get the payment token ID submitted by the form;
 
-    // Strings for translation - optional
-    messages: {
-        validDate: 'valid\ndate', // optional - default 'valid\nthru'
-        monthYear: 'mm/yyyy', // optional - default 'month/year'
-    },
+const token = request.body.stripeToken; //usin express
 
-    // Default placeholders for rendered fields - optional
-    placeholders: {
-        number: '•••• •••• •••• ••••',
-        name: 'Full Name',
-        expiry: '••/••',
-        cvc: '•••'
-    },
-
-    masks: {
-        cardNumber: '•' // optional - mask card number
-    },
-
-    // if true, will log helpful messages for setting up Card
-    debug: false // optional - default false
-});
+const charge = stripe.charges.create({
+  amount:999,
+  currency:'usd',
+  description : 'Example charge',
+  source: token
+})
